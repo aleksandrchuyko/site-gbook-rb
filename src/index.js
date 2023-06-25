51382d1da0834b23d246491148c12f50f0f49a7e
aleksandrchuyko <chao.mess@gmail.com>
2023-06-25 16:56:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/site-gbook/">
+      <BrowserRouter >
         <App />
       </BrowserRouter>
     </PersistGate>
